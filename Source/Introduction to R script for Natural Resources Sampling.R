562b949234a9eee1d819260bc4ee8d8e22ad9bd0
Dan Johnson <djj4tree@gmail.com>
2024-11-12 16:18:53 -0500

    first commit

---
+++ Source/Introduction to R script for Natural Resources Sampling.R
@@ -0,0 +1,15 @@
+## Example script for Natural Resource Sampling
+# November 2024
+# Brought to you in 4-D
+# Dan Johnson
+# Denver Cayetano
+# Diego Rocha
+# D. Alex Bowers
+
+# first we need some toolkits called packages or libraries
+library(ggplot2)  # for making figures
+library(palmerpenguins) # for the data
+
+
+
+
