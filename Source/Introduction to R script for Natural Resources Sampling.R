d82dfeab9f5a7e6dc8caa6244fc1fbc3b1437623
Cayetano <dcayetano@ufl.edu>
2024-11-14 16:26:19 -0500

    Denver's additions -- reorganize by sections, Added objects, tips and resources sections... edited and comments...left notes on thought about what should be removed.
    Feel free to remove whatever...Just my two cents

--- Source/Introduction to R script for Natural Resources Sampling.R
+++ Source/Introduction to R script for Natural Resources Sampling.R
@@ -1,41 +1,116 @@
-## Example script for Natural Resource Sampling
+####------------------------------------------------------------------------####
+## EXAMPLE SCRIPT FOR NATURAL RESOURCE SAMPLING
 # November 2024
 # Brought to you in 4-D
 # Dan Johnson
 # Denver Cayetano
 # Diego Rocha
 # D. Alex Bowers
+####------------------------------------------------------------------------####
 
-# first we need some toolkits called packages or libraries
+
+
+####---- OBJECTIVES ------------------------------------------------------------
+
+# You will get an introduction into how to:
+ # Create objects
+ # View, explore and subset data
+ # Create scatter and box plots with base R and ggplot
+ # Conduct T-test, ANOVA and Linear regression and view results
+
+
+
+####---- TIPS AND TRICKS -------------------------------------------------------  
+
+# Hash tags allow you to comment..this way R doesn't mistake it for code or a function
+# Multiple hash tags allow creation of collapsible sections in the code
+# CTRL + ENTER allow you to run a line of code
+# this symbol <- allow you to assign an object
+# Just as in Excel functions end with ()
+# We can use the dollar sign ($) to select a particular column from data e.g. pen$species
+# Shorter object names saves time typing and reduces typing errors
+# Only use an object name once
+
+
+
+####---- ONLINE RESOURCES TO LEARN R -------------------------------------------
+
+# https://ourcodingclub.github.io/tutorials
+# https://swirlstats.com/
+
+
+
+####---- LOAD PACKAGES/LIBRARIES -----------------------------------------------
+
+# Libraries/package have pre-made tools/functions that help us to work with data and make figures
+# Some functions are built into R and require no libraries/packages
+
 library(ggplot2)  # for making figures
-library(palmerpenguins) # for the data
+library(palmerpenguins) # Gives us some data to work with
 
-# First thing to do is load the data into the session
-# the library already loads the data in but I am going to
-# shorten the name to avoid misspelling penquin
 
-pen <- penguins
 
-# explore the data
+####---- LOAD DATA -------------------------------------------------------------
 
-# We will look at the object 'pen'
+# Let's load some data to play with. 
+# We will use data about penguins from the palmerpenguins package above
+
+pen <- penguins # import penguins data & shorten to "pen" to avoid mispelling
+
+
+
+####---- EXPLORE THE DATA ------------------------------------------------------
+
+# We will look at the object 'pen' with the view function...look at the columns
 View(pen)
 
-# Let's summarize all the columns
+head(pen) # gives you the first six rows of the dataset
+tail(pen) # gives you the last six rows of the dataset
+names(pen) # gives you the names of the columns in your dataset
+
+# Let's look at the number of individuals per species on each island
+table(pen$species, pen$island) # which species occur on which islands?
+
+# Let's summarize all the columns with the summary function
 summary(pen)
 
-# which species occur on which islands?
+# Calculate max, min and mean body mass 
+max(pen$body_mass_g, na.rm = T)
+min(pen$body_mass_g, na.rm = T)
+mean(pen$body_mass_g, na.rm = T)
 
-table(pen$species, pen$island)
 
-# visualize the data
+####--- VISUALIZE THE DATA: SCATTER PLOTS --------------------------------------
+# the plot function create scatter plots of two variables
 
-plot(pen$body_mass_g, pen$bill_length_mm)
-plot(pen$body_mass_g, pen$flipper_length_mm)
-plot(pen$body_mass_g, pen$bill_depth_mm)
+plot(pen$body_mass_g, pen$bill_length_mm) # scatter plot of body mass vs bill length
 
-boxplot(pen$body_mass_g ~ pen$sex)
-boxplot(pen$body_mass_g ~ pen$species)
+plot(pen$body_mass_g, pen$flipper_length_mm) # scatter plot of body mass vs flipper length
+
+plot(pen$body_mass_g, pen$bill_depth_mm) # scatter plot of body mass vs bill depth
+
+# Remember the ggplot2 package we loaded above? This gives prettier figures
+ggplot(data = pen, aes(x = body_mass_g, y = bill_length_mm)) +
+  geom_point() +
+  theme_bw() + # the theme is the style of the plot...try replacing this with theme_classic()
+  labs(x = "Body mass (g)", y = "Bill length (mm)") # Rename the axis
+
+
+
+####--- VISUALIZE THE DATA: BOX PLOTS ------------------------------------------
+
+# The boxplot function creates box plots of a continuous variable by groups
+
+boxplot(pen$body_mass_g ~ pen$sex) # box plot of body mass vs sex
+
+boxplot(pen$body_mass_g ~ pen$species) # box plot of body mass vs sex
+
+# A fancier plot with ggplot -- try adding a theme here
+ggplot(data = pen, aes(x = species, y = body_mass_g)) +
+  geom_boxplot() +
+  labs(x = "Sex", y = "Body mass (g)") # Rename the axis
+
+### FROM DENVER--- I THINK BELOW MIGHT BE TOO MUCH TOO SOON
 boxplot(pen$body_mass_g ~ pen$sex*pen$species)
 boxplot(pen$body_mass_g ~ pen$sex*pen$species*pen$island)
 
@@ -43,32 +118,123 @@ ggplot(pen, aes(x = sex, y = body_mass_g))+
   geom_boxplot()+
   facet_grid(species~island)
 
-# since Adelie penguins occur on all islands lets make an object
-# with just that species
 
+
+####---- DATA ANALYSIS: T-TEST -------------------------------------------------
+
+# Adelie penguins occur on all islands, right?
+table(pen$species, pen$island)
+
+# Let's subset data only for Adelie penguins and save as a different object
 adel <- subset(pen, pen$species == 'Adelie')
 
-# t-test to see if Adelie penguins differ in weight by sex
+# Do Adelie penguins differ in weight by sex? We can answer this with a T-test
+male_adel <- subset(adel, adel$sex=='male') # subset data for males
+female_adel <- subset(adel, adel$sex=='female') # subset data for females
 
-madel <- subset(adel, adel$sex=='male')
-fadel <- subset(adel, adel$sex=='female')
+# view the subsetted data
+View(male_adel)
+View(female_adel)
 
-t.test(x = madel$body_mass_g, y = fadel$body_mass_g)
+# Test for differences with T-test
+t.test(x = male_adel$body_mass_g, y = female_adel$body_mass_g) # is there a significant difference?
 
 
 
-# regression model to see the relation between 
-# body mass and flipper length in Adelie females
+####---- DATA ANALYSIS: ANOVA --------------------------------------------------
 
-mod <- lm(fadel$body_mass_g ~ fadel$flipper_length_mm)
+# Does the weight of male Adelie penguins differ by island?
+View(male_adel)
 
-summary(mod)
-
-ggplot(fadel, aes(x = flipper_length_mm, y = body_mass_g))+
-  geom_point()+
-  geom_smooth(method = 'lm')+
-  labs(x = "Flipper length (mm)", y = "Body mass (g)")+
+ggplot(male_adel, aes(x = island, y = body_mass_g))+
+  geom_boxplot() +
+  labs(x = 'Island', y = "Body mass (g)") +
   theme_bw()
+
+
+# Let's test with an ANOVA -- can be written two ways
+anova_mod <- aov(male_adel$body_mass_g ~ male_adel$island) 
+
+anova_mod <- aov(body_mass_g ~ island, data = male_adel)
+
+summary(anova_mod) # Is there a signicant difference?
+
+
+# Remember that complicated Tukey post-hoc test in excel?
+TukeyHSD(anova_mod)
+
+
+#### FROM DENVER --- ISNT ABOVE THE COMMON APPROACH? LEAVE LM FOR REGRESSION SECTION?
+#mod <- lm(male_adel$body_mass_g ~ male_adel$island)
+#anova(mod)
+
+
+
+####---- DATA ANALYSIS: LINEAR REGRESSION --------------------------------------
+
+# Is there a relationship between body mass and flipper length in female Adelie penguins?
+View(female_adel)
+
+plot(female_adel$body_mass_g, female_adel$bill_length_mm) # scatter plot of body mass vs bill length
+
+# Let's test the relationship with a regression-- can be written two ways
+regression_mod <- lm(female_adel$body_mass_g ~ female_adel$flipper_length_mm)
+
+regression_mod <- lm(body_mass_g ~ flipper_length_mm, data = female_adel)
+
+summary(regression_mod) # Is there a significant relationship?
+
+ggplot(female_adel, aes(x = flipper_length_mm, y = body_mass_g)) +
+  geom_point() +
+  geom_smooth(method = 'lm') + # this adds a trendline like in Excel
+  labs(x = "Flipper length (mm)", y = "Body mass (g)") +
+  theme_bw()
+
+
+
+
+####---- COOL CUSTOM GRAPHS WITH GGPLOT -----------------------------------------------
+
+# ggplot gives you an array of possibilities when plotting, you can change the points 
+# size, shape, colors, the plot theme. Let's try something different. 
+
+ggplot(data = pen, aes(x = flipper_length_mm, y = body_mass_g)) +
+  geom_point(color = "darkgreen", 
+             size = 3) +  # scatter plot of points
+  geom_smooth(method = "lm", 
+              se = FALSE, 
+              color = "red", 
+              size = 1.5) +  # add regression line
+  labs(title = "Flipper Length vs Body Mass",
+       x = "Flipper Length (mm)", 
+       y = "Body Mass (g)") + 
+  theme_minimal()  # minimal theme for a cleaner look
+
+
+# Create the second plot (Flipper Length vs Body Mass) with different customizations
+ggplot(data = pen, aes(x = flipper_length_mm, y = body_mass_g)) +
+  # Change the point shape to a triangle and set the color to blue
+  geom_point(shape = 17, color = "steelblue", size = 4) +  
+  # Add the regression line with a thicker, dashed orange line
+  geom_smooth(method = "lm", se = FALSE, color = "orange", size = 1.5, linetype = "dashed") +  
+  # Customize the labels and title with a more bold font
+  labs(title = "Flipper Length vs Body Mass", 
+       x = "Flipper Length (mm)", 
+       y = "Body Mass (g)") + 
+  theme_classic() +  # Change to classic theme (there's other possibilities as well)
+  # Customize the font and size of the axis labels and title
+  theme(axis.title = element_text(size = 14, face = "bold"), 
+        axis.text = element_text(size = 12), 
+        plot.title = element_text(size = 16, face = "bold", hjust = 0.5))
+
+
+
+
+
+
+#### FROM DENVER -- I think below should be removed. I think subsetting model coefs, 
+# pasting and adding annotations adds a lot more function and arguments that will create confusion
+
 
 # VIEWING THE DATA
 # There is a couple of different ways to explore this data
@@ -162,13 +328,4 @@ ggplot(pen, aes(x = flipper_length_mm, y = body_mass_g)) +
   annotate("text", x = 190, y = 5800, label = r2_label2, color = "black", size = 4)
 
 
-# ANOVA to see if Adelie male penguins differ in weight by island
-
-mod <- lm(madel$body_mass_g~madel$island)
-anova(mod)
-
-ggplot(madel, aes(x = island, y = body_mass_g))+
-  geom_boxplot()+
-  labs(x = 'Island', y = "Body mass (g)")+
-  theme_bw()
 
